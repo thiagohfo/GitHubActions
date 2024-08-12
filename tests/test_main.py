db2b3595b589106769765a9d967cc8213c9b6c6a
thiago <thiagohfo@gmail.com>
2024-08-12 15:11:02 -0300

    Teste de Secret

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,4 +6,4 @@ client = TestClient(app)
 def test_home():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Processo de Software!"}
+    assert response.json() == {"message": "Processo de Software!!"}
